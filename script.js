fc82e9536e2ecfc38c93bc12f3faa501599b9ed9
unknown <vijaysudhakar760@gmail.com>
2024-07-29 00:18:16 +0530

    Updated

--- script.js
+++ script.js
@@ -3,18 +3,34 @@ let closeBtn = document.querySelector("#btn");
 let searchBtn = document.querySelector(".bx-search");
 closeBtn.addEventListener("click", () => {
   maxsidebar.classList.toggle("open");
-  menuBtnChange(); //calling the function(optional)
+  menuBtnChange();
 });
-searchBtn.addEventListener("click", () => {
-  // maxsidebar open when you click on the search iocn
-  maxsidebar.classList.toggle("open");
-  menuBtnChange(); //calling the function(optional)
-});
-// following are the code to change maxsidebar button(optional)
+
 function menuBtnChange() {
   if (maxsidebar.classList.contains("open")) {
-    closeBtn.classList.replace("bx-menu", "bx-menu-alt-right"); //replacing the iocns class
+    closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");
   } else {
-    closeBtn.classList.replace("bx-menu-alt-right", "bx-menu"); //replacing the iocns class
+    closeBtn.classList.replace("bx-menu-alt-right", "bx-menu");
   }
 }
+
+// =========  LTR to RTL =============
+
+const directionToggle = document.getElementById("directionToggle");
+const body = document.body;
+const maxSidebar = document.getElementById("maxsidebar");
+const dashboardSection = document.getElementById("dashboard-section");
+const maxSidebarlogout = document.getElementById("maxSidebarlogout");
+const tooltips = document.querySelectorAll(".tooltip");
+const payrollPlusBtn = document.getElementById("payroll-plus-btn");
+
+directionToggle.addEventListener("click", () => {
+  body.classList.toggle("rtl");
+  maxSidebar.classList.toggle("rlt");
+  dashboardSection.classList.toggle("rlt");
+  maxSidebarlogout.classList.toggle("rlt");
+  payrollPlusBtn.classList.toggle("rlt");
+  tooltips.forEach((tooltip) => {
+    tooltip.classList.toggle("rlt");
+  });
+});
